0d673f871a1d29de65c8092e9ef06ff49927b8dd
Simon Wood <simon@mungewell.org>
2015-03-27 01:54:50 -0600

    Apparently there is a check between 'AUTH_CONST_STRING' and the contents of 'METHODS_TO_ENABLE'
    If either changes, the call will not suceed. Bummer!! Reverted to know set for testing purposes

--- src/example/authenticate.py
+++ src/example/authenticate.py
@@ -8,6 +8,80 @@ import base64, hashlib
 
 AUTH_CONST_STRING = "90adc8515a40558968fe8318b5b023fdd48d3828a2dda8905f3b93a3cd8e58dc"
 
+METHODS_TO_ENABLE = "\
+camera/setFlashMode:\
+camera/getFlashMode:\
+camera/getSupportedFlashMode:\
+camera/getAvailableFlashMode:\
+camera/setExposureCompensation:\
+camera/getExposureCompensation:\
+camera/getSupportedExposureCompensation:\
+camera/getAvailableExposureCompensation:\
+camera/setSteadyMode:\
+camera/getSteadyMode:\
+camera/getSupportedSteadyMode:\
+camera/getAvailableSteadyMode:\
+camera/setViewAngle:\
+camera/getViewAngle:\
+camera/getSupportedViewAngle:\
+camera/getAvailableViewAngle:\
+camera/setMovieQuality:\
+camera/getMovieQuality:\
+camera/getSupportedMovieQuality:\
+camera/getAvailableMovieQuality:\
+camera/setFocusMode:\
+camera/getFocusMode:\
+camera/getSupportedFocusMode:\
+camera/getAvailableFocusMode:\
+camera/setStillSize:\
+camera/getStillSize:\
+camera/getSupportedStillSize:\
+camera/getAvailableStillSize:\
+camera/setBeepMode:\
+camera/getBeepMode:\
+camera/getSupportedBeepMode:\
+camera/getAvailableBeepMode:\
+camera/setCameraFunction:\
+camera/getCameraFunction:\
+camera/getSupportedCameraFunction:\
+camera/getAvailableCameraFunction:\
+camera/setLiveviewSize:\
+camera/getLiveviewSize:\
+camera/getSupportedLiveviewSize:\
+camera/getAvailableLiveviewSize:\
+camera/setTouchAFPosition:\
+camera/getTouchAFPosition:\
+camera/cancelTouchAFPosition:\
+camera/setFNumber:\
+camera/getFNumber:\
+camera/getSupportedFNumber:\
+camera/getAvailableFNumber:\
+camera/setShutterSpeed:\
+camera/getShutterSpeed:\
+camera/getSupportedShutterSpeed:\
+camera/getAvailableShutterSpeed:\
+camera/setIsoSpeedRate:\
+camera/getIsoSpeedRate:\
+camera/getSupportedIsoSpeedRate:\
+camera/getAvailableIsoSpeedRate:\
+camera/setExposureMode:\
+camera/getExposureMode:\
+camera/getSupportedExposureMode:\
+camera/getAvailableExposureMode:\
+camera/setWhiteBalance:\
+camera/getWhiteBalance:\
+camera/getSupportedWhiteBalance:\
+camera/getAvailableWhiteBalance:\
+camera/setProgramShift:\
+camera/getSupportedProgramShift:\
+camera/getStorageInformation:\
+camera/startLiveviewWithSize:\
+camera/startIntervalStillRec:\
+camera/stopIntervalStillRec:\
+camera/actFormatStorage:\
+system/setCurrentTime"
+'''
+
 # Enable everything we know about
 METHODS_TO_ENABLE = "\
 camera/stopMovieRec:\
@@ -148,6 +222,7 @@ camera/getAvailableStillQuality:\
 camera/getStillQuality:\
 camera/setStillQuality:\
 "
+'''
 
 camera = SonyAPI()
 print "API List:", camera.getAvailableApiList()
