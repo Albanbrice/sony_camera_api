0a5a63fff1c23247112b7b6e91e749ef8f3f25cf
Simon Wood <simon@mungewell.org>
2017-10-19 22:43:48 -0600

    Code tidy

--- src/example/authenticate.py
+++ src/example/authenticate.py
@@ -5,9 +5,17 @@
 
 from pysony import SonyAPI, payload_header
 import base64, hashlib
+import six
 
+camera = SonyAPI()
+
+# This call fails with a '403 - Permission Error' on the HDR-AS15 (fw V3.0)
+# Once authenticated it will complete sucessfully
+# print("Get Movie Quality: %s" % camera.getMovieQuality())
+
+
+# Tied to Methods list below, 64 _ASCII_ characters = 256 bit equivilant
 AUTH_CONST_STRING = "90adc8515a40558968fe8318b5b023fdd48d3828a2dda8905f3b93a3cd8e58dc"
-
 METHODS_TO_ENABLE = "\
 camera/setFlashMode:\
 camera/getFlashMode:\
@@ -80,166 +88,21 @@ camera/startIntervalStillRec:\
 camera/stopIntervalStillRec:\
 camera/actFormatStorage:\
 system/setCurrentTime"
-'''
 
-# Enable everything we know about
-METHODS_TO_ENABLE = "\
-camera/stopMovieRec:\
-camera/startMovieRec:\
-camera/stopIntervalStillRec:\
-camera/startIntervalStillRec:\
-camera/stopAudioRec:\
-camera/startAudioRec:\
-camera/getSupportedContShootingSpeed:\
-camera/getAvailableContShootingSpeed:\
-camera/getContShootingSpeed:\
-camera/setContShootingSpeed:\
-camera/getSupportedShutterSpeed:\
-camera/getAvailableShutterSpeed:\
-camera/getShutterSpeed:\
-camera/setShutterSpeed:\
-camera/getSupportedWhiteBalance:\
-camera/getAvailableWhiteBalance:\
-camera/getWhiteBalance:\
-camera/setWhiteBalance:\
-camera/stopRecMode:\
-camera/startRecMode:\
-camera/getSupportedExposureMode:\
-camera/getAvailableExposureMode:\
-camera/getExposureMode:\
-camera/setExposureMode:\
-camera/getSupportedContShootingMode:\
-camera/getAvailableContShootingMode:\
-camera/getContShootingMode:\
-camera/setContShootingMode:\
-camera/getSupportedFlashMode:\
-camera/getAvailableFlashMode:\
-camera/getFlashMode:\
-camera/setFlashMode:\
-camera/getSupportedBeepMode:\
-camera/getAvailableBeepMode:\
-camera/getBeepMode:\
-camera/setBeepMode:\
-camera/getSupportedFocusMode:\
-camera/getAvailableFocusMode:\
-camera/getFocusMode:\
-camera/setFocusMode:\
-camera/getSupportedShootMode:\
-camera/getAvailableShootMode:\
-camera/getShootMode:\
-camera/setShootMode:\
-camera/getSupportedSteadyMode:\
-camera/getAvailableSteadyMode:\
-camera/getSteadyMode:\
-camera/setSteadyMode:\
-camera/actFormatStorage:\
-camera/getSupportedViewAngle:\
-camera/getAvailableViewAngle:\
-camera/getViewAngle:\
-camera/setViewAngle:\
-system/setCurrentTime:\
-camera/actTakePicture:\
-camera/awaitTakePicture:\
-camera/getSupportedIsoSpeedRate:\
-camera/getAvailableIsoSpeedRate:\
-camera/getIsoSpeedRate:\
-camera/setIsoSpeedRate:\
-camera/getSupportedPostviewImageSize:\
-camera/getAvailablePostviewImageSize:\
-camera/getPostviewImageSize:\
-camera/setPostviewImageSize:\
-camera/startLiveviewWithSize:\
-camera/getSupportedStillSize:\
-camera/getAvailableStillSize:\
-camera/getStillSize:\
-camera/setStillSize:\
-camera/getSupportedLiveviewSize:\
-camera/getAvailableLiveviewSize:\
-camera/getLiveviewSize:\
-camera/setLiveviewSize:\
-camera/stopContShooting:\
-camera/startContShooting:\
-camera/setZoomSetting:\
-camera/setFlipSetting:\
-camera/getSupportedColorSetting:\
-camera/getAvailableColorSetting:\
-camera/getColorSetting:\
-camera/setColorSetting:\
-camera/setTvColorSystem:\
-camera/actZoom:\
-camera/getStorageInformation:\
-camera/getSupportedExposureCompensation:\
-camera/getAvailableExposureCompensation:\
-camera/getExposureCompensation:\
-camera/setExposureCompensation:\
-camera/getSupportedSceneSelection:\
-camera/getAvailableSceneSelection:\
-camera/getSceneSelection:\
-camera/setSceneSelection:\
-camera/getSupportedCameraFunction:\
-camera/getAvailableCameraFunction:\
-camera/getCameraFunction:\
-camera/setCameraFunction:\
-camera/cancelTouchAFPosition:\
-camera/getTouchAFPosition:\
-camera/setTouchAFPosition:\
-camera/setLiveviewFrameInfo:\
-camera/getApplicationInfo:\
-camera/getSupportedFNumber:\
-camera/getAvailableFNumber:\
-camera/getFNumber:\
-camera/setFNumber:\
-camera/getSupportedSelfTimer:\
-camera/getAvailableSelfTimer:\
-camera/getSelfTimer:\
-camera/setSelfTimer:\
-camera/cancelHalfPressShutter:\
-camera/actHalfPressShutter:\
-camera/getSupportedTrackingFocus:\
-camera/getAvailableTrackingFocus:\
-camera/cancelTrackingFocus:\
-camera/actTrackingFocus:\
-camera/getTrackingFocus:\
-camera/setTrackingFocus:\
-camera/getSupportedMovieFileFormat:\
-camera/getAvailableMovieFileFormat:\
-camera/getMovieFileFormat:\
-camera/setMovieFileFormat:\
-camera/getSupportedProgramShift:\
-camera/setProgramShift:\
-camera/getContentCount:\
-camera/getSourceList:\
-camera/getEvent:\
-camera/getAvailableApiList:\
-camera/stopLiveview:\
-camera/startLiveview:\
-camera/getSupportedMovieQuality:\
-camera/getAvailableMovieQuality:\
-camera/getMovieQuality:\
-camera/setMovieQuality:\
-camera/getSupportedStillQuality:\
-camera/getAvailableStillQuality:\
-camera/getStillQuality:\
-camera/setStillQuality:\
-"
-'''
-
-camera = SonyAPI()
-print "API List:", camera.getAvailableApiList()
-
-# This call fails with a '403 - Permission Error' on the HDR-AS15 (fw V3.0)
-print "Get Movie Quality:", camera.getMovieQuality()
-
-resp = camera.actEnableMethods([{"methods": "", "developerName": "", "developerID": "", "sg": ""}])
+# Request random nonce from camera
+resp = camera.actEnableMethods([{"methods": "", "developerName": "", \
+    "developerID": "", "sg": ""}])
 dg = resp["result"][0]["dg"]
 
+# Append nonce to AUTH string and hash
 h = hashlib.sha256()
 h.update(bytes(AUTH_CONST_STRING + dg))
 sg = base64.b64encode(h.digest()).decode("UTF-8")
 
-resp = camera.actEnableMethods([{"methods": METHODS_TO_ENABLE, "developerName": "Sony Corporation", \
+# Pass credentials to camera, which will eval with secret method/values
+resp = camera.actEnableMethods([{"methods": METHODS_TO_ENABLE, \
+   "developerName": "Sony Corporation", \
    "developerID": "7DED695E-75AC-4ea9-8A85-E5F8CA0AF2F3", "sg": sg}])
-print "Authenicated:", resp
 
-print "Get Movie Quality:", camera.getMovieQuality()
+print "Authenicated:", resp
 
